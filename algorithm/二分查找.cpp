85a5aba301d4a4bc79206230ce86f3b8db4b4596
Ding Yubo <dingyubo@4paradigm.com>
2020-03-30 00:01:09 +0800

    update

--- algorithm/二分查找.cpp
+++ algorithm/二分查找.cpp
@@ -6,37 +6,23 @@
 
 using namespace std;
 
-// template<typename T>
-// int binarySearch(T arr[], int n, T target) {
-//   int l = 0, r = n - 1;
-//   while(l <= r) {
-//     // int mid = (l + r) / 2;
-//     int mid = l + (r - l) / 2; // 当l+r足够大超过了整形的时候 c++不报错 所以换成减法的形式 才没有bug
-//     if (arr[mid] == target) {
-//       return mid;
-//     }
-//     if (target > arr[mid]) {
-//       l = mid + 1;
-//     } else {
-//       r = mid - 1;
-//     }
-//   }
-//   return -1;
-// }
-
-class Solution {
-public:
-  void moveZeroes(vector<int>& nums) {
-    int j = 0;
-    for (unsigned int i = 0; i < nums.size(); i++) {
-      if (nums[i]) {
-        int temp = nums[j];
-        nums[j++] = nums[i];
-        nums[i] = temp;
-      }
+template<typename T>
+int binarySearch(T arr[], int n, T target) {
+  int l = 0, r = n - 1;
+  while(l <= r) {
+    // int mid = (l + r) / 2;
+    int mid = l + (r - l) / 2; // 当l+r足够大超过了整形的时候 c++不报错 所以换成减法的形式 才没有bug
+    if (arr[mid] == target) {
+      return mid;
+    }
+    if (target > arr[mid]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
     }
   }
-};
+  return -1;
+}
 
 int main(void) {
 }
